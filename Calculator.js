96d6314ea68472d216627907df6e09da640bb7e2
Vincent van Dam <99027519@mydavinci.nl>
2017-01-16 13:17:18 +0100

    no message

--- Calculator.js
+++ Calculator.js
@@ -1,11 +1,13 @@
-var insert = document.getElementById('insert')
+var insert = document.getElementById('insert');
+
 function cijfer1(cijfer){
  console.log(cijfer + " ");
  insert.innerHTML += cijfer;
 }
 
-var answer = document.getElementById('answer')
+var answer = document.getElementById('answer');
+
 function antwoord1(antwoord){
 console.log(antwoord + " ");
-insert.innerHTML += antwoord;
+answer.innerHTML += antwoord;
 }
