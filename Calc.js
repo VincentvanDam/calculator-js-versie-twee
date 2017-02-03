9386cf71f161ed77aec3160c3a0d578ece548212
Vincent van Dam <99027519@mydavinci.nl>
2017-02-03 11:10:37 +0100

    no message

---
+++ Calc.js
@@ -0,0 +1,4 @@
+function test(n1){
+    console.log(test)
+    calculator.txt1.value=calculator.txt1.value+n1;
+}
--- Calculator.js
+++ Calculator.js
@@ -47,28 +47,29 @@ function cijfer(y) {
 
 
 function antwoord1(){
+	debugger;
 	var answer = document.getElementById('answer');
-	answer = "";
 	var op = som[som.length - 1];
 
 	som.push(cyf);
 	cyf = "";
-var num1 = som[som.length - 3];
-var num2 = som[som.length - 1];
+	var num1 = som[som.length - 3];
+	var num2 = som[som.length - 1];
 
-var anns = "";
-if (op == "-") {
-	anns = num1 - num2;
-}else if (op == "+") {
-	anns = parseFloat(num1) + parseFloat(num2);
-}else if (op == "/") {
-	anns = num1 / num2;
-}else if (op == "*") {
-	anns = num1 * num2;
-}
+	var anns = "";
+	if (op == "-") {
+		anns = num1 - num2;
+	}else if (op == "+") {
+		anns = parseFloat(num1) + parseFloat(num2);
+	}else if (op == "/") {
+		anns = num1 / num2;
+	}else if (op == "*") {
+		anns = num1 * num2;
+	}
 
-console.log(anns);
-answer.innerHTML = anns;
+	
+	console.log(anns);
+	answer.innerHTML = anns;
 
 }
 
